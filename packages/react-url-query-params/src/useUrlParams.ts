d27dc00190a91585cd020a49a0b3b20941593be1
pkuzina <pavlo.kuzina@guesty.com>
2025-12-01 09:44:43 +0200

    configured biome

--- packages/react-url-query-params/src/useUrlParams.ts
+++ packages/react-url-query-params/src/useUrlParams.ts
@@ -1,97 +1,91 @@
-import {useSearchParams} from 'react-router-dom'
-import {useCallback, useMemo} from 'react'
-import {upperFirst} from './utils'
-import type {Capitalize, QueryParamConfig} from './types.utils'
+import { useCallback, useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import type { Capitalize, QueryParamConfig } from "./types.utils";
+import { upperFirst } from "./utils";
 
 type QueryParamHookResult<T extends string, O extends string> = {
-  [K in O as `is${Capitalize<T>}${Capitalize<K>}`]: boolean
+  [K in O as `is${Capitalize<T>}${Capitalize<K>}`]: boolean;
 } & {
-  [K in `set${Capitalize<T>}`]: (value: O) => void
+  [K in `set${Capitalize<T>}`]: (value: O) => void;
 } & {
-  [K in T]: O | null
+  [K in T]: O | null;
 } & {
-  [K in `toggle${Capitalize<T>}`]: () => void
+  [K in `toggle${Capitalize<T>}`]: () => void;
 } & {
-  [K in `clear${Capitalize<T>}`]: () => void
-}
+  [K in `clear${Capitalize<T>}`]: () => void;
+};
 
 function useUrlParams<T extends string, O extends string>(
   config: QueryParamConfig<T, O>,
 ): QueryParamHookResult<T, O> {
-  const [searchParams, setSearchParams] = useSearchParams()
-  
-  const currentValue = searchParams.get(config.keyName) as O | null
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentValue = searchParams.get(config.keyName) as O | null;
+
   const setterFunction = useCallback(
     (newValue: O) => {
       if (config.options.includes(newValue)) {
-        const params = new URLSearchParams(searchParams.toString())
-        params.set(config.keyName, newValue)
-        
-        setSearchParams([...params])
-      }
-      
-      if (!newValue) {
-        const params = new URLSearchParams(searchParams.toString())
-        params.delete(config.keyName)
+        const params = new URLSearchParams(searchParams.toString());
+        params.set(config.keyName, newValue);
+
+        setSearchParams([...params]);
       }
     },
     [config.keyName, config.options, searchParams, setSearchParams],
-  )
-  
+  );
+
   const onToggle = useCallback(() => {
     if (config.options.length !== 2) {
-      console.warn('onToggle is only available when there are exactly two options')
-      return
+      console.warn("onToggle is only available when there are exactly two options");
+      return;
     }
-    
-    let currentOptionIndex = config.options.indexOf(currentValue as O)
+
+    const currentOptionIndex = config.options.indexOf(currentValue as O);
     let nextOption: O;
-    
+
     if (currentOptionIndex !== -1) {
-      const nextIndex = (currentOptionIndex + 1) % config.options.length
-      
-      nextOption = config.options[nextIndex]
+      const nextIndex = (currentOptionIndex + 1) % config.options.length;
+
+      nextOption = config.options[nextIndex];
     } else {
-      nextOption = config.options[0]
+      nextOption = config.options[0];
     }
-    
-    setterFunction(nextOption)
-  }, [config.options, currentValue, setterFunction])
-  
+
+    setterFunction(nextOption);
+  }, [config.options, currentValue, setterFunction]);
+
   const clearParam = useCallback(() => {
     const params = new URLSearchParams(searchParams.toString());
-    
+
     if (params.has(config.keyName)) {
-      params.delete(config.keyName)
-      
-      setSearchParams([...params])
+      params.delete(config.keyName);
+
+      setSearchParams([...params]);
     }
-    
-  }, [config.options, searchParams])
-  
+  }, [searchParams, config.keyName, setSearchParams]);
+
   const capitalizedOptions = useMemo(() => {
     return config.options.reduce(
       (acc, option) => {
-        const capitalizedOption = upperFirst(option)
-        const capitalizedKeyName = upperFirst(config.keyName)
-        
+        const capitalizedOption = upperFirst(option);
+        const capitalizedKeyName = upperFirst(config.keyName);
+
         return Object.assign(acc, {
-          [`is${capitalizedKeyName}${capitalizedOption}`]: searchParams.get(config.keyName) === option,
-        })
+          [`is${capitalizedKeyName}${capitalizedOption}`]:
+            searchParams.get(config.keyName) === option,
+        });
       },
       {} as { [key: string]: boolean },
-    )
-  }, [searchParams, config.keyName, config.options])
-  
+    );
+  }, [searchParams, config.keyName, config.options]);
+
   return {
     [config.keyName]: currentValue,
     [`set${upperFirst(config.keyName)}` as const]: setterFunction,
     [`toggle${upperFirst(config.keyName)}` as const]: onToggle,
     [`clear${upperFirst(config.keyName)}` as const]: clearParam,
     ...capitalizedOptions,
-  } as QueryParamHookResult<T, O>
+  } as QueryParamHookResult<T, O>;
 }
 
-export default useUrlParams
-
+export default useUrlParams;
